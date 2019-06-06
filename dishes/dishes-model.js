66e1259249237052de36943ea185aa48222f1b5c
Jonathan Taylor <jonyonson@icloud.com>
2019-06-06 18:18:03 -0500

    mvp finished i think

--- dishes/dishes-model.js
+++ dishes/dishes-model.js
@@ -1,1 +1,31 @@
 const db = require('../data/dbConfig.js');
+
+module.exports = {
+  getDishes,
+  addDish,
+  getDish,
+  getRecipes,
+  addRecipe,
+};
+
+function getDishes() {
+  return db('dishes');
+}
+
+function addDish(dish) {
+  return db('dishes').insert(dish);
+}
+
+function getDish(id) {
+  return db('dishes')
+    .where({ id })
+    .first();
+}
+
+function getRecipes() {
+  return db('recipes');
+}
+
+function addRecipe(recipe) {
+  return db('recipes').insert(recipe);
+}
